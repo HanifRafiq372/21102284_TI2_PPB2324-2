453e26c6ebc9043d33807f2db7804e9acd5748aa
21102284 <21102284@ittelkom-pwt.ac.id>
2024-03-21 11:06:40 +0700

    Praktikum 05 Navigation&Routing

---
+++ Praktikum_05/navigation_dan_routing/lib/home_page.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: overridden_fields
+
+import 'package:flutter/material.dart';
+
+import 'second_page.dart';
+
+class HomePage extends StatelessWidget {
+ // Mengubah key menjadi parameter konstruktor
+ @override
+  final Key? key;
+
+ // Menambahkan parameter key ke konstruktor
+ const HomePage({this.key}) : super(key: key);
+
+ @override
+ Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: const Color.fromARGB(255, 54, 136, 244),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                 context,
+                 MaterialPageRoute(
+                    builder: (context) => const SecondPage(data: 'Data dari Home Page',),
+                 ),
+                );
+              },
+              child: const Text('Menuju halaman kedua'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/second_page', arguments: 'Menggunakan route');
+              },
+              child: const Text('Menuju halaman kedua dengan route'),
+            ),
+          ],
+        ),
+      ),
+    );
+ }
+}
